639122fb35902850f489e148ba15efb12cd50569
NelsonSousaJr <nelsonsousajr@gmail.com>
2021-08-11 20:45:24 -0300

    Alteração no desconto
    coloquei um descontofixo para toda a loja, com um opcional de adicionar
    mais desconto caso necessário

--- Exercicios/src/classe/Produto.java
+++ Exercicios/src/classe/Produto.java
@@ -4,20 +4,23 @@ public class Produto
 {
 	String nome;
 	double preco;
-	double desconto;
+	static final double DESCONTO = 0.25;//quando criamos uma constante por padrão colocamos em caixa alta
 	
 	//OBRIGATORIAMENTE CONSTRUTOR NAO PODE TER RETORNO E NEM TER TIPO DECLARADO
 	
 	Produto (){//Agora eu criei um contrutor, que serve para criar os objetos sem precisar cria-los manualmente, basta eu informar os parametros do construtor
 		//O construtor padrão já vem implicito caso vc n crie um construtor, mas caso crie um, vc precisa declarar tb um construtor padrão caso ainda queira usa-lo
 	}
-	Produto (String nomeInnicial, double precoInicial, double descontoInicial){//o construtor tb é um método porém sem retorno.
+	Produto (String nomeInnicial, double precoInicial){//o construtor tb é um método porém sem retorno.
 	//n se deve declarar o tipo do construtor, caso vc declare ele se torna um metodo comum
 		nome = nomeInnicial;//aqui estamos apenas declarando os valores das variáveis no construtor
 		preco = precoInicial;//ou seja, as informações colocadas no construtor serão passadas para as variáveis automaticamente
-		desconto = descontoInicial;//após criar um construtor é necessario salvar o arquivo para que ele passe a informação para a outra classe
+//		DESCONTO = descontoInicial;//após criar um construtor é necessario salvar o arquivo para que ele passe a informação para a outra classe
 	}
 	double precoComDesconto() {//criação do método para calcular desconto, como a classe produto ja tem preco e desconto, não precisamos colocar no método.
-		return preco * (1 - desconto);
+		return preco * (1 - DESCONTO);
+	}
+	double precoComDesconto(double descontoGerente) {//criação método alternativo, para adicionar mais desconto
+		return preco * (1 - (DESCONTO + descontoGerente));
 	}
 }
--- Exercicios/src/classe/ProdutoTeste.java
+++ Exercicios/src/classe/ProdutoTeste.java
@@ -8,13 +8,13 @@ public class ProdutoTeste
 //		p1.nome = "Notebook";//não precisam ser importados pois pertencem ao mesmo pacote "classe"
 //		p1.preco = 4356.89;
 //		p1.desconto = 0.25;
-		Produto p1 = new Produto("Notebook", 4356.89, 0.25);//porque criei o construtor, agora consigo criar um objeto do tipo produto com apenas uma linha de código
+		Produto p1 = new Produto("Notebook", 4356.8);//porque criei o construtor, agora consigo criar um objeto do tipo produto com apenas uma linha de código
 		
 		
 		var p2 = new Produto();//também podemos declarar como var desde que declaremos o tipo produto.
 		p2.nome = "Caneta Preta";
-		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		p2.preco = 10.00;
+//		p2.desconto = 0.29;
 		
 		System.out.println(p1.nome + ": R$" + p1.preco);
 		System.out.println(p2.nome + ": R$" + p2.preco);
