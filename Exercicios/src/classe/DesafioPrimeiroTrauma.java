e7e12594cf939ad61d0499e0463b17f25c65d3d8
NelsonSousaJr <nelsonsousajr@gmail.com>
2021-08-11 23:33:09 -0300

    Desafio primeiro trauma
    Desafio para acessar um atributo da instancia(Objeto) dentro da
    classe(static).

--- Exercicios/src/classe/DesafioPrimeiroTrauma.java
+++ Exercicios/src/classe/DesafioPrimeiroTrauma.java
@@ -1,5 +1,13 @@
 package classe;
 
 public class DesafioPrimeiroTrauma {
-
+		int a = 3;//para acessar esse valor é preciso
+		static int b = 5;//ou especifico que a variável pertence à classe (static)
+	public static void main(String[] args) {
+		
+		DesafioPrimeiroTrauma p = new DesafioPrimeiroTrauma();//criar uma instancia, nesse caso p
+		System.out.println(p.a);//a é um atributo da instancia e p é uma variável da instancia
+		System.out.println(b);//nesse caso basta 
+		
+	}
 }
