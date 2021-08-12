d232319f449bb229ed580e332baf56b546f4ee9f
NelsonSousaJr <nelsonsousajr@gmail.com>
2021-08-12 01:56:32 -0300

    this. this()
    Aula sobre as funções de this. e this() que puxam atributos da instancia

--- Exercicios/src/classe/Data.java
+++ Exercicios/src/classe/Data.java
@@ -5,19 +5,21 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
-
+	//THIS() PUXA TODOS OS ATRIBUTOS DA INSTANCIA
+	//THIS. PUXA UMA POR VEZ
 	Data(){//como criei um construtor, precisei criar o construtor padrão para poder utilizá-lo
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+//		dia = 1;
+//		mes = 1;
+//		ano = 1970;
+		this(1, 1, 1970);//this () tb chama instancia
 	}
-	Data(int diaInicial, int mesInicial, int anoInicial){
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	Data(int dia, int mes, int ano){//variáveis da classe
+		this.dia = dia;//aqui diferenciamos a instancia das variáveis da classe com o this.
+		this.mes = mes;
+		this.ano = ano;
 	}
-	String dataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+	String dataFormatada() {//função da instancia
+		return String.format("%d/%d/%d", this.dia, mes, ano);//aqui ele puxa atributos da instancia n faz diferença se colocar o this.
 	}
 
 }
